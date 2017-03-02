055dfe39b684c514c77474f2ed1b1644e9d47959
Armin Eslami <armin.eslami@gmail.com>
2017-03-01 23:14:40 -0500

    chore: Setup 'grunt-eslint' task in 'gruntfile.js'
    the plugin identifies and reports on errors in JS code.
    
    [ticket: #2]

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,15 @@
 var gulp = require('gulp');
+var eslint = require('gulp-eslint');
 
-gulp.task('default', function() {
-    // TODO: add task
+gulp.task('lint', function() {
+    return gulp.src('src/js/*.js')
+    .pipe(eslint())
+    .pipe(eslint.format())
+    .pipe(eslint.failAfterError());
+});
+
+gulp.watch('src/js/*.js', ['lint']);
+
+gulp.task('default', ['lint'], function() {
+    // This will only run if the lint task is successful...
 });
