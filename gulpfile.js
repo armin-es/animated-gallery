0d22888dc53e23032ece823b3bf25665f3cd294e
Armin Eslami <armin.eslami@gmail.com>
2017-03-02 05:00:22 -0500

    chore: Setup 'gulp-concat' for JS files in 'gulpfile.js'
    concatenates all JS files.
    
    [ticket: #2]

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,8 @@
 var gulp = require('gulp');
 var eslint = require('gulp-eslint');
+var concat = require('gulp-concat');
 
+//JS Lint Task:
 gulp.task('lint', function() {
     return gulp.src('src/js/*.js')
     .pipe(eslint())
@@ -8,8 +10,17 @@ gulp.task('lint', function() {
     .pipe(eslint.failAfterError());
 });
 
-gulp.watch('src/js/*.js', ['lint']);
-
-gulp.task('default', ['lint'], function() {
-    // This will only run if the lint task is successful...
+//Concat. JS files Task:
+gulp.task('concat-js', function() {
+    return gulp.src(['./node_modules/jquery/dist/jquery.js', 'src/js/jquery.quicksand.js', './node_modules/jquery-fancybox/source/**/*.js', 'script.js'])
+    .pipe(concat('main.js'))
+    .pipe(gulp.dest('./src/js/'));
 });
+
+// Watch Task:
+gulp.task('watch', function() {
+    gulp.watch('src/js/*.js', ['lint']);
+});
+
+// Default Task:
+gulp.task('default', ['lint', 'watch']);
